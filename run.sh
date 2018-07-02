fb0bbb0978d09d15d5a4f12aa501c6241aeb460c
ShakeJumper <dimitr95@mail.ru>
2018-07-02 18:47:36 +0300

    push project

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+source ~/tensorflow/bin/activate
+
 python evaluate.py -i /Users/dmitryoleynik/PycharmProjects/SuperResolutionCNN/increase_2x
 python evaluate.py -p /Users/dmitryoleynik/PycharmProjects/SuperResolutionCNN/predict_2x
+
+deactivate
